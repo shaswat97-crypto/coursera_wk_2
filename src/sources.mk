dffa5f2b6641fbf5feae87ec8cf6fcb8bea20c12
shaswat97-crypto <shaswat97@gmail.com>
2022-03-06 12:16:08 +0530

    made a skeleton layout for Makefile and sources.mk

---
+++ src/sources.mk
@@ -0,0 +1,36 @@
+#******************************************************************************
+# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+#
+# Redistribution, modification or use of this software in source or binary
+# forms is permitted as long as the files maintain this copyright. Users are 
+# permitted to modify this and use it to learn about the field of embedded
+# software. Alex Fosdick and the University of Colorado are not liable for any
+# misuse of this material. 
+#
+#*****************************************************************************
+
+# Add your Source files to this variable
+ifeq ($(PLATFORM),HOST)
+aSOURCES =\
+	 memory.c\
+	 main.c
+
+else
+aSOURCES =\
+	 interrupts_msp432p401r_gcc.c\
+	 system_msp432p401r.c\
+	 startup_msp432p401r_gcc.c\
+	 memory.c\
+	 main.c
+
+endif
+
+# Add your include paths to this variable
+INCLUDES = \
+	   -I ~/ese-coursera-course1/assessments/m2/include/common\
+	   -I ~/ese-coursera-course1/assessments/m2/include/msp432\
+	   -I ~/ese-coursera-course1/assessments/m2/include/CMSIS\
+	   -I ~/ese-coursera-course1/assessments/m2
+
+
+
